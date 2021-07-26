3431cf6717b671ca2b4030263f76ee78c822210b
Nicolas Dupuis <nicolas86200@gmail.com>
2021-07-26 10:10:15 +0200

    Version non corrigé

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 import "./App.css";
-import Button from "./components/Button/button";
 
 function App() {
-  return (
-    <div className="App">
-      <Button
-        texte="TOTO"
-        couleurBordure="#123"
-        couleurFond="red"
-        couleurTexte="yellow"
-      />
-    </div>
-  );
+  return <div className="App">{/* Tester le bouton ici :) */}</div>;
 }
 
 export default App;

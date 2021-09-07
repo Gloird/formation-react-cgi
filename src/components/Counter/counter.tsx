bdb9b5a711bc7669f15fc18b677abc55f23bd883
Nicolas Dupuis <nicolas86200@gmail.com>
2021-09-07 08:14:51 +0200

    Version non corrigé

--- src/components/Counter/counter.tsx
+++ src/components/Counter/counter.tsx
@@ -1,31 +1,50 @@
-import React, { FC, useState } from "react";
+import React, { Component } from "react";
 import Button from "../Button/button";
 
-const Counter: FC = () => {
-  const [counter, setCounter] = useState(0);
+//Création d'un interface pour le state
+interface CounterState {
+  counter: number;
+}
 
-  return (
-    <div>
-      <Button
-        texte="+1"
-        onClick={() => {
-          //Il est important d'utiliser un callbakc pour metre a jour le stat,
-          //car dans ce cas la l'utilisateur peux spam le bouton est le state
-          //doit bien faire +1 ou -1 avec le nombre qui ce trouve dans le state a ce moment là
-          setCounter((state) => state + 1);
-        }}
-      />
-      {counter}
-      <Button
-        texte="-1"
-        onClick={() => {
-          //Il est important d'utiliser un callbakc pour metre a jour le stat,
-          //car dans ce cas la l'utilisateur peux spam le bouton est le state
-          //doit bien faire +1 ou -1 avec le nombre qui ce trouve dans le state a ce moment là
-          setCounter((state) => state - 1);
-        }}
-      />
-    </div>
-  );
-};
+//ICI pas de props donc on met {}
+class Counter extends Component<{}, CounterState> {
+  //Ici pas de type/interface pour le props donc on met {}
+  constructor(props: {}) {
+    //Il est obligatoir d'appeler le super avec au moins les props
+    super(props);
+    //Initialisation du sate
+    this.state = { counter: 0 };
+  }
+
+  render() {
+    return (
+      <div>
+        <Button
+          texte="+1"
+          onClick={() => {
+            //Il est important d'utiliser un callbakc pour metre a jour le stat,
+            //car dans ce cas la l'utilisateur peux spam le bouton est le state
+            //doit bien faire +1 ou -1 avec le nombre qui ce trouve dans le state a ce moment là
+            this.setState((state) => {
+              return { counter: state.counter + 1 };
+            });
+          }}
+        />
+        {this.state.counter}
+        <Button
+          texte="-1"
+          onClick={() => {
+            //Il est important d'utiliser un callbakc pour metre a jour le stat,
+            //car dans ce cas la l'utilisateur peux spam le bouton est le state
+            //doit bien faire +1 ou -1 avec le nombre qui ce trouve dans le state a ce moment là
+            this.setState((state) => {
+              return { counter: state.counter - 1 };
+            });
+          }}
+        />
+      </div>
+    );
+  }
+}
+
 export default Counter;
